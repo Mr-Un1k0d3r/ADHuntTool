f7fcae2480d553c2978b0f0e3c0922b0452544ec
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2021-07-28 11:00:41 -0400

    Update ADHuntTool.cs

--- ADHuntTool.cs
+++ ADHuntTool.cs
@@ -13,6 +13,7 @@ using System.IO;
 using System.Collections;
 using System.Security.Principal;
 using System.ServiceProcess;
+using System.Security.AccessControl;
 
 namespace ADHuntTool
 {
@@ -36,6 +37,8 @@ namespace ADHuntTool
                 return shi1_netname;
             }
         }
+
+        public static bool showACL = false;
 
         [DllImport("kernel32.dll")]
         public static extern uint GetLastError();
@@ -250,7 +253,14 @@ namespace ADHuntTool
             Int32 size = r.Count;
             for (Int32 i = 0; i < size; i++)
             {
-                sb.Append(r[i] + ",");
+                if(r[i].GetType().ToString() == "System.Byte[]") {
+                    sb.Append(Encoding.ASCII.GetString((byte[])r[i]) + ",");
+                } else
+                {
+                    sb.Append(r[i] + ",");
+                }
+
+                
             }
             return sb.ToString().TrimEnd(',');
         }
@@ -267,9 +277,9 @@ namespace ADHuntTool
         static string FormatCertFlag(string flag)
         {
             StringBuilder sb = new StringBuilder();
-            UInt32 flags = UInt32.Parse(flag); 
+            UInt32 flags = UInt32.Parse(flag);
 
-            if((flags & 0x01) == 0x01)
+            if ((flags & 0x01) == 0x01)
             {
                 sb.Append("CT_FLAG_ENROLLEE_SUPPLIES_SUBJECT,");
             }
@@ -324,6 +334,22 @@ namespace ADHuntTool
 
             return sb.ToString();
         }
+
+        static string FormatSDDL(ResultPropertyValueCollection r)
+        {
+            StringBuilder sb = new StringBuilder();
+            Int32 size = r.Count;
+            for (Int32 i = 0; i < size; i++)
+            {
+                RawSecurityDescriptor raw = new RawSecurityDescriptor((byte[])r[i], 0);
+
+               
+                sb.Append(SDDLParser.Parse(raw.GetSddlForm(AccessControlSections.All)) + ",");
+                
+
+            }
+            return sb.ToString().TrimEnd(',');
+        }
         static List<string> LdapQuery(string domain, string query, string properties, bool showNull = true, bool returnList = false, string prepend = "LDAP://")
         {
             domain = prepend + domain;
@@ -338,11 +364,22 @@ namespace ADHuntTool
             ds.Filter = query;
             ds.PageSize = Int32.MaxValue;
 
+            if(Program.showACL)
+            {
+                ds.SecurityMasks = SecurityMasks.Dacl | SecurityMasks.Group;
+            }
+
             foreach (SearchResult r in ds.FindAll())
             {
                 try
                 {
                     StringBuilder sb = new StringBuilder();
+                    if(Program.showACL)
+                    {
+                        sb.Append("ntSecurityDescriptor" + new string(' ', 24 - "ntSecurityDescriptor".Length) + ": ");
+                        sb.Append(FormatSDDL(r.Properties["ntSecurityDescriptor"]));
+                        sb.Append("\r\n");
+                    }
                     foreach (string prop in properties.Split(','))
                     {
                         if (prop.ToLower().StartsWith("managed") && r.Properties[prop].Count <= 0)
@@ -361,15 +398,16 @@ namespace ADHuntTool
                                 sb.Append(prop + new string(' ', 24 - prop.Length) + ": ");
                             }
 
-                            if(prop == "msPKI-Certificate-Name-Flag")
+                            if (prop == "msPKI-Certificate-Name-Flag")
                             {
                                 sb.Append(FormatCertFlag(r.Properties[prop][0].ToString()));
-                            } else
+                            }
+                            else
                             {
                                 sb.Append(item > 1 ? "[" + FormatProperties(r.Properties[prop]) + "]" : FormatTime(r.Properties[prop][0]));
                                 sb.Append("\r\n");
                             }
-                            
+
                             if (returnList)
                             {
                                 output.Add(r.Properties[prop][0].ToString());
@@ -456,6 +494,8 @@ namespace ADHuntTool
         static void Main(string[] args)
         {
             bool verboseDebug = Array.Exists(args, match => match.ToLower() == "-verbose");
+            Program.showACL = Array.Exists(args, match => match.ToLower() == "-acl");
+
             ThreadPool.SetMaxThreads(max_threadpool, max_threadpool);
 
             // ShowWindow(GetConsoleWindow(), 0);
@@ -1081,6 +1121,233 @@ namespace ADHuntTool
                     Console.WriteLine("Usage: {0} options domain [arguments]", System.Reflection.Assembly.GetExecutingAssembly().Location);
                 }
             }
+        }
+    }
+
+    // Thanks stackoverflow for this parser.
+    class SDDLParser
+    {
+        static private Dictionary<string, string> ACE_Types = null;
+        static private Dictionary<string, string> ACE_Flags = null;
+        static private Dictionary<string, string> Permissions = null;
+        static private Dictionary<string, string> Trustee = null;
+
+        private static void Initialize()
+        {
+            ACE_Types = new Dictionary<string, string>();
+            ACE_Flags = new Dictionary<string, string>();
+            Permissions = new Dictionary<string, string>();
+            Trustee = new Dictionary<string, string>();
+            #region Add ACE_Types
+            ACE_Types.Add("A", "Access Allowed");
+            ACE_Types.Add("D", "Access Denied");
+            ACE_Types.Add("OA", "Object Access Allowed");
+            ACE_Types.Add("OD", "Object Access Denied");
+            ACE_Types.Add("AU", "System Audit");
+            ACE_Types.Add("AL", "System Alarm");
+            ACE_Types.Add("OU", "Object System Audit");
+            ACE_Types.Add("OL", "Object System Alarm");
+            #endregion
+            #region Add ACE_Flags
+            ACE_Flags.Add("CI", "Container Inherit");
+            ACE_Flags.Add("OI", "Object Inherit");
+            ACE_Flags.Add("NP", "No Propagate");
+            ACE_Flags.Add("IO", "Inheritance Only");
+            ACE_Flags.Add("ID", "Inherited");
+            ACE_Flags.Add("SA", "Successful Access Audit");
+            ACE_Flags.Add("FA", "Failed Access Audit");
+            #endregion
+            #region Add Permissions
+            #region Generic Access Rights
+            Permissions.Add("GA", "Generic All");
+            Permissions.Add("GR", "Generic Read");
+            Permissions.Add("GW", "Generic Write");
+            Permissions.Add("GX", "Generic Execute");
+            #endregion
+            #region Directory Access Rights
+            Permissions.Add("RC", "Read Permissions");
+            Permissions.Add("SD", "Delete");
+            Permissions.Add("WD", "Modify Permissions");
+            Permissions.Add("WO", "Modify Owner");
+            Permissions.Add("RP", "Read All Properties");
+            Permissions.Add("WP", "Write All Properties");
+            Permissions.Add("CC", "Create All Child Objects");
+            Permissions.Add("DC", "Delete All Child Objects");
+            Permissions.Add("LC", "List Contents");
+            Permissions.Add("SW", "All Validated Writes");
+            Permissions.Add("LO", "List Object");
+            Permissions.Add("DT", "Delete Subtree");
+            Permissions.Add("CR", "All Extended Rights");
+            #endregion
+            #region File Access Rights
+            Permissions.Add("FA", "File All Access");
+            Permissions.Add("FR", "File Generic Read");
+            Permissions.Add("FW", "File Generic Write");
+            Permissions.Add("FX", "File Generic Execute");
+            #endregion
+            #region Registry Key Access Rights
+            Permissions.Add("KA", "Key All Access");
+            Permissions.Add("KR", "Key Read");
+            Permissions.Add("KW", "Key Write");
+            Permissions.Add("KX", "Key Execute");
+            #endregion
+            #endregion
+            #region Add Trustee's
+            Trustee.Add("AO", "Account Operators");
+            Trustee.Add("RU", "Alias to allow previous Windows 2000");
+            Trustee.Add("AN", "Anonymous Logon");
+            Trustee.Add("AU", "Authenticated Users");
+            Trustee.Add("BA", "Built-in Administrators");
+            Trustee.Add("BG", "Built in Guests");
+            Trustee.Add("BO", "Backup Operators");
+            Trustee.Add("BU", "Built-in Users");
+            Trustee.Add("CA", "Certificate Server Administrators");
+            Trustee.Add("CG", "Creator Group");
+            Trustee.Add("CO", "Creator Owner");
+            Trustee.Add("DA", "Domain Administrators");
+            Trustee.Add("DC", "Domain Computers");
+            Trustee.Add("DD", "Domain Controllers");
+            Trustee.Add("DG", "Domain Guests");
+            Trustee.Add("DU", "Domain Users");
+            Trustee.Add("EA", "Enterprise Administrators");
+            Trustee.Add("ED", "Enterprise Domain Controllers");
+            Trustee.Add("WD", "Everyone");
+            Trustee.Add("PA", "Group Policy Administrators");
+            Trustee.Add("IU", "Interactively logged-on user");
+            Trustee.Add("LA", "Local Administrator");
+            Trustee.Add("LG", "Local Guest");
+            Trustee.Add("LS", "Local Service Account");
+            Trustee.Add("SY", "Local System");
+            Trustee.Add("NU", "Network Logon User");
+            Trustee.Add("NO", "Network Configuration Operators");
+            Trustee.Add("NS", "Network Service Account");
+            Trustee.Add("PO", "Printer Operators");
+            Trustee.Add("PS", "Self");
+            Trustee.Add("PU", "Power Users");
+            Trustee.Add("RS", "RAS Servers group");
+            Trustee.Add("RD", "Terminal Server Users");
+            Trustee.Add("RE", "Replicator");
+            Trustee.Add("RC", "Restricted Code");
+            Trustee.Add("SA", "Schema Administrators");
+            Trustee.Add("SO", "Server Operators");
+            Trustee.Add("SU", "Service Logon User");
+            #endregion
+        }
+
+        private static string friendlyTrusteeName(string trustee)
+        {
+            if (Trustee.ContainsKey(trustee))
+            {
+                return Trustee[trustee];
+            }
+            else
+            {
+                try
+                {
+                    System.Security.Principal.SecurityIdentifier sid = new System.Security.Principal.SecurityIdentifier(trustee);
+                    return sid.Translate(typeof(System.Security.Principal.NTAccount)).ToString();
+                }
+                catch (Exception)
+                {
+                    return trustee;
+                }
+            }
+        }
+
+        private static string doParse(string subSDDL, string Separator, string Separator2)
+        {
+            string retval = "";
+            char type = subSDDL.ToCharArray()[0];
+            if (type == 'O')
+            {
+                string owner = subSDDL.Substring(2);
+                return "Owner: " + friendlyTrusteeName(owner) + Separator;
+            }
+            else if (type == 'G')
+            {
+                string group = subSDDL.Substring(2);
+                return "Group: " + friendlyTrusteeName(group) + Separator;
+            }
+            else if ((type == 'D') || (type == 'S'))
+            {
+                if (type == 'D')
+                {
+                    retval += "DACL" + Separator;
+                }
+                else
+                {
+                    retval += "SACL" + Separator;
+                }
+                string[] sections = subSDDL.Split('(');
+                for (int count = 1; count < sections.Length; count++)
+                {
+                    retval += "------------" + Separator;
+                    string[] parts = sections[count].TrimEnd(')').Split(';');
+                    retval += "";
+                    if (ACE_Types.ContainsKey(parts[0]))
+                    {
+                        retval += Separator2 + "Type: " + ACE_Types[parts[0]] + Separator;
+                    }
+                    if (ACE_Flags.ContainsKey(parts[1]))
+                    {
+                        retval += Separator2 + "Inheritance: " + ACE_Flags[parts[1]] + Separator;
+                    }
+                    for (int count2 = 0; count2 < parts[2].Length; count2 += 2)
+                    {
+                        string perm = parts[2].Substring(count2, 2);
+                        if (Permissions.ContainsKey(perm))
+                        {
+                            if (count2 == 0)
+                            {
+                                retval += Separator2 + "Permissions: " + Permissions[perm];
+                            }
+                            else
+                            {
+                                retval += "|" + Permissions[perm];
+                            }
+                        }
+                    }
+                    retval += Separator;
+                    retval += Separator2 + "Trustee: " + friendlyTrusteeName(parts[5]) + Separator;
+                }
+            }
+            return retval;
+        }
+
+        public static string Parse(string SDDL)
+        {
+            return Parse(SDDL, "\r\n", "");
+        }
+
+        public static string Parse(string SDDL, string Separator, string Separator2)
+        {
+            string retval = "";
+            if (ACE_Types == null)
+            {
+                Initialize();
+            }
+            int startindex = 0;
+            int nextindex = 0;
+            int first = 0;
+            string section;
+            while (true)
+            {
+                first = SDDL.IndexOf(':', nextindex) - 1;
+                startindex = nextindex;
+                if (first < 0)
+                {
+                    break;
+                }
+                if (first != 0)
+                {
+                    section = SDDL.Substring(startindex - 2, first - startindex + 2);
+                    retval += doParse(section, Separator, Separator2);
+                }
+                nextindex = first + 2;
+            }
+            section = SDDL.Substring(startindex - 2);
+            retval += doParse(section, Separator, Separator2);
+            return retval;
         }
     }
 }
